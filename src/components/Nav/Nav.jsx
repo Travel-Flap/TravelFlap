d4f9286e13f89e617368633b197d60680c637cc1
firstname lastname <Info@apptroit.com>
2022-10-01 16:39:13 +0530

    image path changes

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -177,7 +177,7 @@ export const Nav = () => {
         
         
         <Link to="/home"><div className='travelflapmainlogo'>
-              <img style={{height:"50px",width:"150px"}} src="images/travelFlapLogo.jpg" alt="travelflap logo" />
+              <img style={{height:"50px",width:"150px"}} src="Images/TravelFlapLogo.jpg" alt="travelflap logo" />
                 <FaBars style={{width:"30px",height:"30px"}} className='hamburgermenuicon' onClick={getMenu}/>
         </div>
         </Link>  
