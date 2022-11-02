1ff0ea5a2f8e4a4463c07855602f7c34bee50eb6
firstname lastname <Info@apptroit.com>
2022-11-02 12:05:59 +0530

    code changes 02-11-2022

--- src/components/Pages/NewStaycationPage/NewStaycationStaticBanners/NewStaycationStaticBanners.jsx
+++ src/components/Pages/NewStaycationPage/NewStaycationStaticBanners/NewStaycationStaticBanners.jsx
@@ -8,32 +8,32 @@ export const NewStaycationStaticBanners = () => {
        <div className="NewStaycationStaticBannersmainDiv">
          <div className="NewStaycationStaticBannersDivOne">
             <div className="NewStaycationStaticBannerImageOne">
-              <img src="images/StaycationSataticbannerImages/staycation2.jpg" />
+              <img src="Images/StaycationStaticbannerImages/staycation2.jpg" />
             </div>
             <div className="NewStaycationStaticBannerImageOne">
-              <img src="images/StaycationSataticbannerImages/staycation3.jpg" />
+              <img src="Images/StaycationStaticbannerImages/staycation3.jpg" />
             </div>
             <div className="NewStaycationStaticBannerImageOne">
-              <img src="images/StaycationSataticbannerImages/staycation4.jpg" />
+              <img src="Images/StaycationSataticbannerImages/staycation4.jpg" />
             </div>
          </div>
          <div className="NewStaycationStaticBannersDivTwo">
              <div className="NewStaycationStaticBannerImageTwo">
-               <img src="images/StaycationSataticbannerImages/staycation8.png" />
+               <img src="Images/StaycationSataticbannerImages/staycation8.png" />
              </div>
              <div className="NewStaycationStaticBannerImageTwo">
-               <img src="images/StaycationSataticbannerImages/staycation1.jpg" />
+               <img src="Images/StaycationSataticbannerImages/staycation1.jpg" />
              </div>
          </div>
          <div className="NewStaycationStaticBannersDivThree">
             <div className="NewStaycationStaticBannerImageOne">
-               <img src="images/StaycationSataticbannerImages/staycation5.jpg" />
+               <img src="Images/StaycationSataticbannerImages/staycation5.jpg" />
             </div>
             <div className="NewStaycationStaticBannerImageOne">
-               <img src="images/StaycationSataticbannerImages/staycation6.jpg" />
+               <img src="Images/StaycationSataticbannerImages/staycation6.jpg" />
             </div>
             <div className="NewStaycationStaticBannerImageOne">
-               <img src="images/StaycationSataticbannerImages/staycation7.png" />
+               <img src="Images/StaycationSataticbannerImages/staycation7.png" />
             </div>
          </div>
        </div>
